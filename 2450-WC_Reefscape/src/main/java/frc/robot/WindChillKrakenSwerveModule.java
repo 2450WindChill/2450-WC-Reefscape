a3593d31c49b8544994b234f7af6344cd414a982
Mikael Coenen <coenenmikael@gmail.com>
2025-01-27 19:16:17 -0600

    Close to finishing Kraken conversion

--- 2450-WC_Reefscape/src/main/java/frc/robot/WindChillKrakenSwerveModule.java
+++ 2450-WC_Reefscape/src/main/java/frc/robot/WindChillKrakenSwerveModule.java
@@ -32,7 +32,6 @@ public class WindChillKrakenSwerveModule {
   private TalonFXConfiguration talonFXConfigs;
   private CurrentLimitsConfigs currentConfigs;
   private VoltageConfigs voltageConfigs;
-  
 
   private RelativeEncoder driveEncoder;
   private RelativeEncoder integratedAngleEncoder;
@@ -54,13 +53,10 @@ public class WindChillKrakenSwerveModule {
     configAngleMotor();
 
     /* Drive Motor Config */
-    driveMotor = new TalonFX(moduleConstants.driveMotorID)
-    driveEncoder = driveMotor.getEncoder();
-    driveController = driveMotor.getClosedLoopController();
+    driveMotor = new TalonFX(moduleConstants.driveMotorID);
+
     configDriveMotor();
-
     lastAngle = getState().angle;
-
   }
 
   public void setDesiredState(SwerveModuleState desiredState, boolean isSlowMode) {
@@ -88,17 +84,17 @@ public class WindChillKrakenSwerveModule {
     double desiredAngle = angle.getDegrees();
 
     // while (desiredAngle < 0) {
-    //   desiredAngle += 360;
+    // desiredAngle += 360;
     // }
 
     // while (desiredAngle > 360) {
-    //   desiredAngle -= 360;
+    // desiredAngle -= 360;
     // }
 
     SmartDashboard.putNumber("Desired Angle " + moduleNumber, desiredAngle);
 
     angleController.setReference(desiredAngle, SparkMax.ControlType.kPosition);
-    lastAngle = angle;    
+    lastAngle = angle;
   }
 
   public void resetToAbsolute() {
@@ -123,67 +119,81 @@ public class WindChillKrakenSwerveModule {
     return angleEncoder.getAbsolutePosition().getValueAsDouble();
   }
 
+  // TODO: Once we get Kraken encoder this should work (done?)
   public double getDriveEncoder() {
-    return driveEncoder.getPosition();
+    return driveMotor.getPosition().getValueAsDouble();
   }
 
+  // TODO: Once we get Kraken encoder this should work (done?)
   public SwerveModuleState getState() {
-    return new SwerveModuleState(driveEncoder.getVelocity(), getAngle());
+    return new SwerveModuleState(driveMotor.getVelocity().getValueAsDouble(), getAngle());
   }
 
+  // TODO: Once we get Kraken encoder this should work (done?)
   public SwerveModuleState getAutoState() {
-    return new SwerveModuleState(driveEncoder.getVelocity(), getAutoAngle());
+    return new SwerveModuleState(driveMotor.getVelocity().getValueAsDouble(), getAutoAngle());
   }
 
   private void configAngleMotor() {
 
     angleConfig = new SparkMaxConfig();
-    angleConfig 
-      .smartCurrentLimit(Constants.angleContinuousCurrentLimit)
-      .inverted(Constants.angleInvert)
-      .voltageCompensation(Constants.voltageComp);
+    angleConfig
+        .smartCurrentLimit(Constants.angleContinuousCurrentLimit)
+        .inverted(Constants.angleInvert)
+        .voltageCompensation(Constants.voltageComp);
     angleConfig.encoder
-      .positionConversionFactor(Constants.angleConversionFactor);
+        .positionConversionFactor(Constants.angleConversionFactor);
     angleConfig.closedLoop
-      // Wrap angle motors to stay within 0-360 degrees
-      .positionWrappingEnabled(true)
-      .positionWrappingMinInput(0.0)
-      .positionWrappingMaxInput(360.0)
-      .pidf(Constants.angleKP, Constants.angleKI, Constants.angleKD, Constants.angleKFF);
-    
-      angleMotor.configure(angleConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+        // Wrap angle motors to stay within 0-360 degrees
+        .positionWrappingEnabled(true)
+        .positionWrappingMinInput(0.0)
+        .positionWrappingMaxInput(360.0)
+        .pidf(Constants.angleKP, Constants.angleKI, Constants.angleKD, Constants.angleKFF);
+
+    angleMotor.configure(angleConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
   }
 
+  // TODO: Need to figure out kraken version of configs
   private void configDriveMotor() {
+
+    var talonFXConfigurator = driveMotor.getConfigurator();
+    var talonFXConfigs = new TalonFXConfiguration();
+
+    // enable stator current limit
+    var limitConfigs = new CurrentLimitsConfigs();
+    limitConfigs.StatorCurrentLimit = 120;
+    limitConfigs.StatorCurrentLimitEnable = true;
+    talonFXConfigurator.apply(limitConfigs);
+
+   // Probably not solution
+    driveMotor.setVoltage(Constants.voltageComp);
     
-    talonFXConfigs = new TalonFXConfiguration();
-
-    currentConfigs = new CurrentLimitsConfigs();
-
-    voltageConfigs = new VoltageConfigs();
+    driveMotor.setPosition(0.0);
 
 
-    talonFXConfigs 
-      .smartCurrentLimit(Constants.driveContinuousCurrentLimit)
-      .idleMode(Constants.driveIdleMode)
-      .inverted(Constants.driveInvert)
-      .voltageCompensation(Constants.voltageComp);
+// ------------------------------------------------------------
+    talonFXConfigs
+        //.smartCurrentLimit(Constants.driveContinuousCurrentLimit)
+        .idleMode(Constants.driveIdleMode) // Need?
+        .inverted(Constants.driveInvert) // Need?
+        .voltageCompensation(Constants.voltageComp); // Check what code I put
     driveConfig.encoder
-      .positionConversionFactor(Constants.driveConversionPositionFactor)
-      .velocityConversionFactor(Constants.driveConversionVelocityFactor);
+        .positionConversionFactor(Constants.driveConversionPositionFactor)
+        .velocityConversionFactor(Constants.driveConversionVelocityFactor);
     driveConfig.closedLoop
 
-      .pidf(Constants.driveKP, Constants.driveKI, Constants.driveKD, Constants.driveKFF);
-    
-      driveMotor.configure(driveConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+        .pidf(Constants.driveKP, Constants.driveKI, Constants.driveKD, Constants.driveKFF);
 
-      driveEncoder.setPosition(0.0);
+    driveMotor.configure(driveConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+
+    driveEncoder.setPosition(0.0);
   }
 
   // public SwerveModulePosition getPosition() {
-  //   return new SwerveModulePosition(
-  //       -((getDriveEncoder() / Constants.rotationsPerOneFoot) * Constants.feetToMeters),
-  //       Rotation2d.fromDegrees(getCanCoderInDegrees()));
+  // return new SwerveModulePosition(
+  // -((getDriveEncoder() / Constants.rotationsPerOneFoot) *
+  // Constants.feetToMeters),
+  // Rotation2d.fromDegrees(getCanCoderInDegrees()));
   // }
 
   public SwerveModulePosition getPosition() {
@@ -192,9 +202,7 @@ public class WindChillKrakenSwerveModule {
         Rotation2d.fromDegrees(getCanCoderInDegrees()));
   }
 
-
   public void setPosition(double position) {
     integratedAngleEncoder.setPosition(position);
   }
 }
- 
