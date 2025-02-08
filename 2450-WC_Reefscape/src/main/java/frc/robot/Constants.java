8790cbfebab24c35527563d81bc22adf6b8612a0
Mikael Coenen <coenenmikael@gmail.com>
2025-02-08 13:19:06 -0600

    Swerve fix

--- 2450-WC_Reefscape/src/main/java/frc/robot/Constants.java
+++ 2450-WC_Reefscape/src/main/java/frc/robot/Constants.java
@@ -64,7 +64,7 @@ public final class Constants {
 
   public static final double feetToMeters = 0.3048;
 
-  public static final double driveGearRatio = (8.14 / 1.0); // 6.75:1
+  public static final double driveGearRatio = (6.12 / 1.0); // 6.12:1
   public static final double angleGearRatio = (150 / 7); // 12.8:1
 
   public static final double driveBaseRadius = (Math.sqrt((trackWidth * trackWidth) + (trackWidth * trackWidth)))/2;
