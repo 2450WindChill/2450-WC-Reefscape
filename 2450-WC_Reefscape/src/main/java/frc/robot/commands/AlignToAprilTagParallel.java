6f66efdddfae269fcfc2abf7ce8aa0ff564f118b
J Haider <111470547+jakehaid@users.noreply.github.com>
2025-02-22 09:56:14 -0600

    Started Parallel

--- 2450-WC_Reefscape/src/main/java/frc/robot/commands/AlignToAprilTagParallel.java
+++ 2450-WC_Reefscape/src/main/java/frc/robot/commands/AlignToAprilTagParallel.java
@@ -1,26 +1,49 @@
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import frc.robot.subsystems.DrivetrainSubsystem;
 import frc.robot.subsystems.VisionSubsystem;
 
-public class AlignToAprilTagParallel extends ParallelCommandGroup {
+public class AlignToAprilTagParallel extends Command {
 
     VisionSubsystem m_VisionSubsystem;
     DrivetrainSubsystem m_drivetrainSubsystem;
     double m_strafeTarget;
     double m_approachTarget;
 
+    double tolerance = Math.toRadians(1);
+    double currAngle;
+    double rotSpeed = 0;
+
+    double strafeTolerance = 0.05;
+    double strafeError;
+    double strafeSpeed = 0;
+
+    double approachTolerance = 0.05;
+    double approachError;
+    double approachSpeed = 0;
+
     public AlignToAprilTagParallel(VisionSubsystem visionSubsystem, DrivetrainSubsystem drivetrainSubsystem, double strafeTarget, double approachTarget) {
         m_VisionSubsystem = visionSubsystem;
         m_drivetrainSubsystem = drivetrainSubsystem;
         m_strafeTarget = strafeTarget;
         m_approachTarget = approachTarget;
+    }
 
-        addCommands(
-            new SquareToAprilTag(m_VisionSubsystem, drivetrainSubsystem),
-            new StrafeToAprilTag(m_VisionSubsystem, drivetrainSubsystem, m_strafeTarget),
-            new ApproachAprilTag(m_VisionSubsystem, drivetrainSubsystem, m_approachTarget)
-        );
+    public void initialize() {
+
+    }
+
+    public void execute() {
+
+    }
+
+    public boolean isFinished() {
+        return false;
+    }
+
+    public void end(boolean isFinished) {
+
     }
 }
