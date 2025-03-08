a3b3580f9a08de18b9a0bda59e9161a7f17304d2
J Haider <103543538+IsaacVanOosterom@users.noreply.github.com>
2025-03-08 15:01:32 -0600

    Added MoveToPose

---
+++ 2450-WC_Reefscape/src/main/java/frc/robot/commands/MoveToPose.java
@@ -0,0 +1,86 @@
+package frc.robot.commands;
+
+import java.util.function.BooleanSupplier;
+
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.trajectory.TrapezoidProfile.Constraints;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Constants;
+import frc.robot.subsystems.DrivetrainSubsystem;
+
+public class MoveToPose extends Command {
+
+    DrivetrainSubsystem m_drivetrainSubsystem;
+
+    ProfiledPIDController xController = new ProfiledPIDController(3, 0, 0, new Constraints(Constants.maxSpeed, 2));
+    ProfiledPIDController yController = new ProfiledPIDController(3, 0, 0, new Constraints(Constants.maxSpeed, 2));
+    ProfiledPIDController rotController = new ProfiledPIDController(6, 0, 0, new Constraints(Constants.maxAngularVelocity, 4));
+
+    BooleanSupplier m_overrideSupplier;
+
+    double driveTolerance = 0.05;
+    double rotTolerance = Math.toRadians(3);
+
+    double xTarget;
+    double yTarget;
+    double rotTarget;
+
+    double xSpeed;
+    double ySpeed;
+    double rotSpeed;
+
+    public MoveToPose(DrivetrainSubsystem drivetrainSubsystem, Pose2d target, BooleanSupplier overrideSupplier) {
+        m_drivetrainSubsystem = drivetrainSubsystem;
+        m_overrideSupplier = overrideSupplier;
+        xTarget = target.getX();
+        yTarget = target.getY();
+        rotTarget = target.getRotation().getRadians();
+    }
+
+    public void initialize() {
+        Pose2d initialPose = m_drivetrainSubsystem.getThisPose();
+        xController.reset(initialPose.getX());
+        yController.reset(initialPose.getY());
+        rotController.reset(initialPose.getRotation().getRadians());
+
+        xController.setTolerance(driveTolerance);
+        yController.setTolerance(driveTolerance);
+        rotController.setTolerance(rotTolerance);
+    }
+
+    public void execute() {
+        Pose2d currPose = m_drivetrainSubsystem.getThisPose();
+
+        if (!xController.atGoal()) {
+            xSpeed = xController.calculate(currPose.getX());
+        } else {
+            xSpeed = 0;
+        }
+
+        if (!yController.atGoal()) {
+            ySpeed = xController.calculate(currPose.getY());
+        } else {
+            ySpeed = 0;
+        }
+
+        if (!rotController.atGoal()) {
+            rotSpeed = xController.calculate(currPose.getRotation().getRadians());
+        } else {
+            rotSpeed = 0;
+        }
+
+        m_drivetrainSubsystem.drive(new Translation2d(xSpeed, ySpeed), rotSpeed, false, false);
+    }
+
+    // TODO Change is finished from testing with return false to actual isFinished
+    public boolean isFinished() {
+        return false;
+        // return (xController.atGoal() && yController.atGoal() && rotController.atGoal()) || m_overrideSupplier.getAsBoolean();
+    }
+
+    public void end(boolean isFinished) {
+        m_drivetrainSubsystem.drive(new Translation2d(0, 0), 0, false, false);
+    }
+}
